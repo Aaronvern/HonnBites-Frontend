eb0b5ed38d958e8bfb35e4037ae2e43ce757dba9
Aaronvern <aaronvernekar@gmail.com>
2024-08-16 18:37:05 +0530

    add changes to file structure

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/Layout"
 
 const AppRoutes = () => {
     return (
         <>
         <Routes>
-            <Route path="/" element={<span>HELLO HOME PAGE</span>}></Route>
+            <Route path="/" element={<Layout>Home Page</Layout>}></Route>
             <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>}></Route>
             <Route path="*" element={<Navigate to="/" />}></Route>
         </Routes> 
