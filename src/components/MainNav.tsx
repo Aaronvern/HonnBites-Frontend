2e76ed0e8d29136a7485c9430f53872397b67cde
Aaronvern <aaronvernekar@gmail.com>
2024-08-16 23:52:05 +0530

    add authentication services

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,9 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react"
 import { Button } from "./ui/button"
 
 function MainNav() {
+  const {loginWithRedirect} = useAuth0()
   return (
-    <Button variant="ghost" className=" hover:text-orange-800 text-orange-600 text-xl hover:bg-white font-bold">
-        Login
+    <Button 
+      variant="ghost" 
+      className=" hover:text-orange-800 text-orange-600 text-xl hover:bg-white font-bold"
+      onClick={ async()=> await loginWithRedirect()}
+    >
+     Login
     </Button>
   )
 }
