617a1e6132714055ae399c12bf754a1ff6f13a52
Aaronvern <aaronvernekar@gmail.com>
2024-09-16 02:20:59 +0530

    added create restaurant request api

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,10 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
-
 export default function ManageRestaurantPage() {
+  const { isLoading, createRestaurant } = useCreateMyRestaurant();
+
   return (
-    <ManageRestaurantForm/>
-  )
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 }
