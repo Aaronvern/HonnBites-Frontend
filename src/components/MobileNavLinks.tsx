f0430a9a5afb19cab576fae5bf17c525fa8c86b4
Aaronvern <aaronvernekar@gmail.com>
2024-09-17 19:07:53 +0530

    added all search features -pagination , searchreultcard, populating search result info and sorting option drop down also cuisine filter

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -3,21 +3,29 @@ import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
 export default function MobileNavLinks() {
-  const { logout } = useAuth0()
+  const { logout } = useAuth0();
   return (
     <>
-      
-      <Link to="/user-profile" className="flex justify-center text-xl font-bold hover:text-orange-500">
-      User Profile
+      <Link
+        to="/user-profile"
+        className="flex justify-center text-xl font-bold hover:text-orange-500"
+      >
+        User Profile
       </Link>
-      
-      <Button className="bg-orange-500 flex text-center px-3 font-extrabold text-xl py-5  hover:bg-red-500 "
-        onClick={()=> logout() }
-        >
+
+      <Link
+        to="/manage-restaurant"
+        className="flex justify-center text-xl font-bold hover:text-orange-500"
+      >
+        Manage Restaurant
+      </Link>
+
+      <Button
+        className="bg-orange-500 flex text-center px-3 font-extrabold text-xl py-5  hover:bg-red-500 "
+        onClick={() => logout()}
+      >
         logout
       </Button>
-      
     </>
-  )
+  );
 }
-
